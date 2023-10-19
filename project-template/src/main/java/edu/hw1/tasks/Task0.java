a1cdd2058fee3ac9180b2d574b23099deb472e88
Lisi4ka59 <122356446+Lisi4ka59@users.noreply.github.com>
2023-10-19 16:40:34 +0300

    hw1 initial commit

---
+++ project-template/src/main/java/edu/hw1/tasks/Task0.java
@@ -0,0 +1,11 @@
+package edu.hw1.tasks;
+
+import java.util.logging.Logger;
+
+public class Task0 {
+        private static final Logger LOGGER = Logger.getLogger(Task0.class.getName());
+
+        public static void task0() {
+            LOGGER.info("Привет, мир!");
+        }
+}
---
+++ project-template/src/main/java/edu/hw1/tasks/Task2.java
@@ -0,0 +1,17 @@
+package edu.hw1.tasks;
+
+
+public class Task2 {
+    public static Integer task2(String inputs) {
+        long input = Math.abs(Long.parseLong(inputs.replaceFirst("\\.", "")));
+        int res = 0;
+        if (input == 0) {
+            return 1;
+        }
+        while (input != 0) {
+            input = input / 10;
+            res++;
+        }
+        return res;
+    }
+}
