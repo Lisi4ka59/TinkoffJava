28bc9aff4b06900e971b3441ec9e25b67883c299
Lisi4ka59 <122356446+Lisi4ka59@users.noreply.github.com>
2023-11-13 23:54:29 +0300

    hw 5 bug fixes

--- project-template/src/main/java/edu/hw5/Task7.java
+++ project-template/src/main/java/edu/hw5/Task7.java
@@ -7,19 +7,18 @@ public class Task7 {
     public static class StringValidator {
 
         public static boolean validateString(String input) {
-            // Проверка на содержание не менее 3 символов, включая 0 на третьей позиции
-            Pattern pattern1 = Pattern.compile("^(1*0){2,}[01]*$");
-            Matcher matcher1 = pattern1.matcher(input);
+            String[] patterns = {
+                "^(?:[01]*0){2}[01]*$",
+                "^(0[01]*0|1[01]*1|[01])$",
+                "^[01]{1,3}$"
+            };
 
-            // Проверка на начале и окончание строки одним и тем же символом
-            Pattern pattern2 = Pattern.compile("^(0|1)(1|0)*\\1$");
-            Matcher matcher2 = pattern2.matcher(input);
-
-            // Проверка на длину строки от 1 до 3 символов
-            Pattern pattern3 = Pattern.compile("^[01]{1,3}$");
-            Matcher matcher3 = pattern3.matcher(input);
-
-            return matcher1.matches() && matcher2.matches() && matcher3.matches();
+            for (String pattern : patterns) {
+                if (input.matches(pattern)) {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
--- project-template/src/test/java/StringValidatorTest.java
+++ project-template/src/test/java/StringValidatorTest.java
@@ -6,21 +6,18 @@ import static org.junit.Assert.assertFalse;
 public class StringValidatorTest {
 
     @Test
-    public void testValidStrings() {
-        assertTrue(Task7.StringValidator.validateString("010")); // Должно вернуть true
-        assertTrue(Task7.StringValidator.validateString("000")); // Должно вернуть true
+    public void testMatchingStrings() {
+        assertTrue(Task7.StringValidator.validateString("010101")); // Должно вернуть true
+        assertTrue(Task7.StringValidator.validateString("0"));      // Должно вернуть true
+        assertTrue(Task7.StringValidator.validateString("101"));    // Должно вернуть true
+        assertTrue(Task7.StringValidator.validateString("111"));    // Должно вернуть true
     }
 
     @Test
-    public void testInvalidStrings() {
-        assertFalse(Task7.StringValidator.validateString("01")); // Должно вернуть false
-        assertFalse(Task7.StringValidator.validateString("0000")); // Должно вернуть false
-        assertFalse(Task7.StringValidator.validateString("1010")); // Должно вернуть false
-        assertFalse(Task7.StringValidator.validateString("11")); // Должно вернуть false
-    }
-
-    @Test
-    public void testEmptyString() {
-        assertFalse(Task7.StringValidator.validateString("")); // Должно вернуть false
+    public void testNonMatchingStrings() {
+        assertFalse(Task7.StringValidator.validateString("1110"));    // Должно вернуть false
+        assertFalse(Task7.StringValidator.validateString("11110"));  // Должно вернуть false
+        assertFalse(Task7.StringValidator.validateString(""));       // Должно вернуть false
     }
 }
+
